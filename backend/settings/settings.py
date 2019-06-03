5f2b1395bab8362911f1bb089b1c9cfb58423993
Marvin <villanueva.marvin@gmail.com>
2019-06-03 18:56:16 +0800

    update settings

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tasksdb'
+        'NAME': 'tasksdb',
         'HOST': 'localhost',
         'USER': 'manager',
         'PASSWORD': 'Japs@3261'
