3064a7ee09f8b51eb624cc0c4a539c0dd43287a1
Marvin <villanueva.marvin@gmail.com>
2019-08-28 21:11:17 +0800

    update run shell

--- run.sh
+++ run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 source ./venv/Scripts/activate
 source ./env.sh
-# pipenv shell
 
-# python manage.py collectstatic --noinput
-# python manage.py makemigrations    
-# python manage.py migrate
-# python manage.py runserver --settings=backend.settings.prod
+python manage.py collectstatic --noinput
+python manage.py makemigrations    
+python manage.py migrate
+python manage.py runserver --settings=backend.settings.prod
 
-# case $1 in
-#   dev)
-#     python manage.py runserver --settings=backend.settings.dev
-#     ;;
-#   prod)
-#   	python manage.py collectstatic --noinput
-#     python manage.py makemigrations    
-#     python manage.py migrate
-#     python manage.py runserver --settings=backend.settings.prod
-#     ;;
-# esac
+case $1 in
+  dev)
+    python manage.py runserver --settings=backend.settings.dev
+    ;;
+  prod)
+  	python manage.py collectstatic --noinput
+    python manage.py makemigrations    
+    python manage.py migrate
+    python manage.py runserver --settings=backend.settings.prod
+    ;;
+esac
