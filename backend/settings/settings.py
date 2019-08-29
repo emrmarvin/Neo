54e29ae5d2eac674bb7d9aacf24935772a0e2542
Marvin <villanueva.marvin@gmail.com>
2019-08-29 23:23:10 +0800

    set debug to false

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -23,7 +23,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECRET_KEY = 'nh63*3c!n@x2-um)#bdjk%5oej^+$b$3#!6ib2zn9)=m&yb^8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'finalcontrol-contacts-stg.azurewebsites.net']
 
