0d1a60616c3630b187db39d197bfcdb7761647df
Marvin <villanueva.marvin@gmail.com>
2019-08-29 23:16:40 +0800

    add app url and localhost to allowed host

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nh63*3c!n@x2-um)#bdjk%5oej^+$b$3#!6ib2zn9)=m&yb^8-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'finalcontrol-contacts-stg.azurewebsites.net']
 
 # Application definition
 
