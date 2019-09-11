e84396f4078517629492de4c43670399c1d92ca8
Valdez <Hezekiah.Valdez@Emerson.com>
2019-09-11 19:53:04 +0800

    ace update 09-11-2019 7:52pm

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -10,85 +10,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-import { validationMixin } from 'vuelidate';
-import { required, maxLength, email } from 'vuelidate/lib/validators';
-
-export default {
-  mixins: [validationMixin],
-
-  validations: {
-    name: { required, maxLength: maxLength(10) },
-    email: { required, email },
-    select: { required },
-    checkbox: {
-      checked(val) {
-        return val;
-      },
-    },
-  },
-
-  data: () => ({
-    name: '',
-    email: '',
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-    ],
-    checkbox: false,
-  }),
-
-  computed: {
-    checkboxErrors() {
-      const errors = [];
-      if (!this.$v.checkbox.$dirty) return errors;
-      !this.$v.checkbox.checked && errors.push('You must agree to continue!');
-      return errors;
-    },
-    selectErrors() {
-      const errors = [];
-      if (!this.$v.select.$dirty) return errors;
-      !this.$v.select.required && errors.push('Item is required');
-      return errors;
-    },
-    nameErrors() {
-      const errors = [];
-      if (!this.$v.name.$dirty) return errors;
-      !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long');
-      !this.$v.name.required && errors.push('Name is required.');
-      return errors;
-    },
-    emailErrors() {
-      const errors = [];
-      if (!this.$v.email.$dirty) return errors;
-      !this.$v.email.email && errors.push('Must be valid e-mail');
-      !this.$v.email.required && errors.push('E-mail is required');
-      return errors;
-    },
-  },
-
-  methods: {
-    submit() {
-      this.$v.$touch();
-    },
-    clear() {
-      this.$v.$reset();
-      this.name = '';
-      this.email = '';
-      this.select = null;
-      this.checkbox = false;
-    },
-  },
-};
-</script>
-=======
->>>>>>> 5910430515c3619546e66e0c27dff2c96bb34fbd
 export default {
    name: 'my-component',
    data(){
@@ -126,8 +48,3 @@ export default {
    },
 };
 </script>
-<<<<<<< HEAD
-
-=======
->>>>>>> ace37e5f141c64b4fca94d3022c9b898d6853d77
->>>>>>> 5910430515c3619546e66e0c27dff2c96bb34fbd
