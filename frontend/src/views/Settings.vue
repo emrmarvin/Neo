72632fe15ac4a3c4efe3d2e945a3bad35d01b512
Marvin <villanueva.marvin@gmail.com>
2019-09-18 22:09:15 +0800

    commit

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,20 +1,10 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <h1>Settings</h1>
-=======
   <div class="projects">
     <h1 class="subheading grey--text">Settings</h1>
->>>>>>> b8cbce545a22279db80e58273c2048ffc5c58140
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-  export default {
-
-  }
-=======
 
 export default {
   data() {
@@ -23,5 +13,4 @@ export default {
     };
   },
 };
->>>>>>> b8cbce545a22279db80e58273c2048ffc5c58140
 </script>
