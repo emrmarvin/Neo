50427f05f4502f30d72e31342d40dda70ad1b0cd
emrmarvin <villanueva.marvin@gmail.com>
2019-09-20 01:58:57 +0800

    include frontend dist folder for deployment

---
+++ frontend/dist/js/chunk-2d0daf2f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0daf2f"],{"6e7a":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Feedback")])])}],r={},u=r,l=t("2877"),s=Object(l["a"])(u,c,a,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0daf2f.js.map
